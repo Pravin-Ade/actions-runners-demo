db9c7a47790609512be1acc61acd50a23a0de58c
Pravin-Ade <prav.ade17@gmail.com>
2024-05-21 15:18:51 +0530

    feet{github-action-runners-demo}:update python files

--- src/addition.py
+++ src/addition.py
@@ -1,10 +1,16 @@
 # app.py
 # This is a test commit
-def add(a, b):
-    return a + b
+# def add(1, 2):
+#     return a + b
 
-def test_add():
-    assert add(1, 2) == 3
-    assert add(1, -1) == 0
+# def test_add():
+#     assert add(1, 2) == 3
+#     assert add(1, -1) == 0
 
 # print("Testing the addition.py file")
+
+num1 = 1.5
+num2 = 6.3
+sum = num1 + num2
+print(f"The sum of {num1} and {num2} is {sum:.1f}")
+
